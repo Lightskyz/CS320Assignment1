96d2abc7f413a349a90867070b4dfde280482515
Antoine Rakotozafy <antoine.rakotozafy@gmail.com>
2016-09-05 23:52:44 -0700

    prog1_4 ok, can launch it with arguments

--- prog1_4.c
+++ prog1_4.c
@@ -1,34 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-int main() {
+int main(int argc, char const *argv[]) {
 
-  char name[] = "Antoine Rakotozafy";
-  char email[] = "antoine.rakotozafy@gmail.com";
+  if( argc == 3 ) {
+      char name[] = "Antoine Rakotozafy";
+      char email[] = "antoine.rakotozafy@gmail.com";
 
-  int i;
-  double n;
-  double s = 0.0;
-  double step;
-  double x;
-  double y;
-  double PI = 3.141;
+      double n = atof(argv[1]);
+      double step = atof(argv[2]);
 
-  printf("Assignment #1-4, %s, %s \n", name, email);
+      int i;
 
-  scanf("%lf %lf", &n, &step);
+      double s = 0.0;
+      double x;
+      double y;
+      double PI = 3.141;
 
-  for (i = 0; i < n; i++) {
-     printf("%.2lf  ", x);
-     x = x + step;
-  };
-  printf("\n");
+      printf("Assignment #1-4, %s, %s \n", name, email);
 
-  for (i = 0; i < n; i++) {
-    s = y*PI/180;
-    printf("%.2lf  ", cos(s));
-    y = y + step;
-  };
-  printf("\n");
+      for (i = 0; i < n; i++) {
+         printf("%.2lf  ", x);
+         x = x + step;
+      };
+      printf("\n");
 
+      for (i = 0; i < n; i++) {
+        s = y*PI/180;
+        printf("%.2lf  ", cos(s));
+        y = y + step;
+      };
+      printf("\n");
+   }
+
+   else if( argc > 3 ) {
+      printf("Too many arguments supplied.\n");
+   }
+   else {
+      printf("Two arguments expected.\n");
+   }
 }
