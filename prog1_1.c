9b48cfb18ac8986f6370938211600d3a5184a185
Antoine Rakotozafy <antoine.rakotozafy@gmail.com>
2016-09-05 17:02:42 -0700

    prog1_1 ok

---
+++ prog1_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+  char name[] = "Antoine Rakotozafy";
+  char email[] = "antoine.rakotozafy@gmail.com";
+
+  printf("Assignment #1-1, %s, %s \n", name, email);
+  printf("My name is %s \n", name);
+
+}
